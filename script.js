5c6ad5066dc92f92157f1233dbd1963eab7d0bd0
faizz-k <smartrahman0@gmail.com>
2024-12-29 12:46:45 +0530

    finished

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ let Slides = document.querySelectorAll(".slide");
 let prevBtn = document.querySelector(".prev");
 let nextBtn = document.querySelector(".next");
 
-nextBtn.addEventListener("click",function() {
+function next(){
     let nextSlide = (currentSlide+1)%Slides.length;
     Slides[currentSlide].style.display="none";
 
     Slides[nextSlide].style.display ="block";
     currentSlide = nextSlide;
-})
+}
+
+nextBtn.addEventListener("click",next)
 
 prevBtn.addEventListener("click", function() {
     let prevSlide = currentSlide == 0?Slides.length -1:currentSlide -1;
@@ -20,3 +22,5 @@ prevBtn.addEventListener("click", function() {
     Slides[prevSlide].style.display ="block";
     currentSlide = prevSlide;
 })
+
+setInterval(next,3000)
